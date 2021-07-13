0f016aa20412ac7f58c70482d3eb852c779c7210
Henry0804 <heglamm@gmail.com>
2021-07-13 15:37:03 -0500

    Added test command.
    This command simply runs through 5 different equations checking to see if the output matches. This will eventually break.

--- src/com/Math/Main.java
+++ src/com/Math/Main.java
@@ -13,7 +13,17 @@ public class Main {
       System.out.print("Calc > ");
       String input = scan.nextLine();
       if (input.equals("exit")) {System.exit(0);}
-      if (input.equals("debug")) {debug++;if (debug==4) {debug = 0;};System.out.println("Debug mode is: "+debug);} else {
+
+      if (input.equals("test") ) {
+        int v = 0;
+        v+=Test("1+1+1",3f);
+        v+=Test("2*3*4",24f);
+        v+=Test("2**3",8f);
+        v+=Test("1/2",0.5f);
+        v+=Test("5**8-7*(90)/3-84+6",390337f);
+        System.out.println(v+"/5 tests succeeded.");
+      }
+      else if (input.equals("debug")) {debug++;if (debug==4) {debug = 0;};System.out.println("Debug mode is: "+debug);} else {
         switch(debug) {
           case 0: System.out.println("Result is: "+  Parser.QuickParse(Parser.Parse(Parser.ToNodes(Lexer.Parse(input))))  );break;//Output
           case 1: System.out.println("Token output: "+ Lexer.Parse(input));break;
@@ -24,18 +34,18 @@ public class Main {
 
       }
 
+
+
     }
 
   }
 
-  public static void test() {
-    ArrayList<Token> tokens = Lexer.Parse("1+1*2");
-    ArrayList<Node> nodes = Parser.ToNodes(tokens);
-    System.out.println(nodes);
-    Parser.ConvertFactors(nodes);
-    System.out.println(nodes);
-    Parser.ConvertExpression(nodes);
-    System.out.println(nodes);
-    System.out.println(Parser.QuickParse(nodes));
+  public static int Test(String val, float expected) {
+    float outVal = Parser.QuickParse(Parser.Parse(Parser.ToNodes(Lexer.Parse(val))));
+    boolean v = outVal==expected;
+    String ret = "error";
+    if (v) {ret = "Test suceeded.";} else {ret = "Test failed, value "+expected+" was expected, but returned "+outVal+". ("+val+")";}
+    System.out.println(ret);
+    if (v) {return 1;} else {return 0;}
   }
 }
